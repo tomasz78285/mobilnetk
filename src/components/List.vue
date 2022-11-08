59fe2ef8e3ed0a796c50f66d04ce7c263bd8a35c
Tomek <kad.tom@wp.pl>
2022-11-08 18:11:04 +0100

    Inicjalizacja projektu

---
+++ src/components/List.vue
@@ -0,0 +1,41 @@
+<template>
+    <table>
+        <thead>
+            <tr>
+                <td>Name</td>
+                <td>Code</td>
+                <td>Country</td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="it in italy" :key="it.id">
+                <td>{{ it.name }}</td>
+                <td>{{ it.code }}</td>
+                <td>{{ it.country }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+export default {
+    name: "list",
+    data() {
+        return {
+            italy: []
+        };
+    },
+    created() {
+        fetch("http://localhost:3000/italy")
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            this.italy = data;
+        });
+    }
+};
+</script>
+
+<style>
+</style>
