68c9afaa1b53f5f2339f06786365f52037077a19
Tomek <kad.tom@wp.pl>
2022-11-30 00:05:54 +0100

    Pobranie danych z json

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,38 +1,48 @@
 <template>
-    <table>
-        <thead>
-            <tr>
-                <td>Name</td>
-                <td>Code</td>
-                <td>Country</td>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="it in italy" :key="it.id">
-                <td>{{ it.name }}</td>
-                <td>{{ it.code }}</td>
-                <td>{{ it.country }}</td>
-            </tr>
-        </tbody>
-    </table>
+    <div v-for="club in clubs" :key="club.id">
+        <div>{{ club.name }}</div>
+        <div>{{ club.code }}</div>
+        <div>{{ club.country }}</div>
+        <div>{{ club.year_of_creation }}</div>
+        <div>{{ club.owner }}</div>
+        <div>{{ club.coach }}</div>
+        <div>{{ club.address.city }}</div>
+        <div>{{ club.address.street }}</div>
+        <div>{{ club.address.number }}</div>
+        <div>{{ club.stadium.name }}</div>
+        <div>{{ club.stadium.capacity }}</div>
+        <img :src="club.img" />
+        <p>-------------------------------------</p>
+    </div>
 </template>
 
 <script>
+
+import axios from 'axios';
+
 export default {
     name: "list",
     data() {
         return {
-            italy: []
+            clubs: []
         };
     },
     created() {
-        fetch("http://localhost:3000/italy")
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            this.italy = data;
-        });
+        this.loadData();
+    },
+    methods: {
+        loadData: function () {
+            var vm = this;
+            axios.get('https://raw.githubusercontent.com/tomasz78285/mobilnetk/main/db.json')
+            .then(function (response) {
+               // console.log(response.data.clubs[0].address[0].city);
+                vm.clubs = response.data.clubs;
+            })
+            .catch(function (error) {
+                vm.clubs = 'An error occured.' + error;
+            });
+            
+        }
     }
 };
 </script>
